a3caba3c944eedcca7099566f5255a1a2c15ca03
Avi-17 <workhorseavi17@gmail.com>
2024-11-24 03:55:34 +0530

    deleted useless files

--- bs.js
+++
@@ -1,8 +0,0 @@
-const updatedSecondPrimary = await pool.query(
-    "UPDATE Users SET linkedId = ?, linkPrecedence = ? WHERE id = ?",
-    [firstPrimary, "secondary", secondPrimary]
-);
-const updateSecondPrimaryDescendants = await pool.query(
-    "UPDATE Users SET linkedId = ? WHERE linkedId = ?",
-    [firstPrimary, secondPrimary]
-);
